a85786e4fd2999a1c7df93bfe54a42d00ca7c482
anes-webdev <anes.webdev@gmail.com>
2026-01-15 11:36:42 +0330

    fix: handle admin role page access

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,17 +6,23 @@ import PollsList from '../pages/poll/list';
 import CreatePoll from '../pages/poll/add';
 import PollLink from '../pages/poll/link/PollLink';
 import NotFoundPage from '../pages/not-found/NotFoundPage';
+import { useAuth } from '../hooks/useAuth';
 
 const AppRoutes = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <Routes>
       <Route path={APP_ROUTES.SIGN_IN} element={<SignIn />} />
-      <Route path={APP_ROUTES.POLLS} element={<PollsList />} />
       <Route path={APP_ROUTES.LANDING} element={<Landing />} />
-      <Route path={APP_ROUTES.ADD_POLL} element={<CreatePoll />} />
-      <Route path={APP_ROUTES.EDIT_POLL.PATH} element={<CreatePoll />} />
       <Route path={APP_ROUTES.POLL_LINK.PATH} element={<PollLink />} />
       {/* <Route path="/pollDetails/:pollLink" element={<PollDetailsPage />} /> */}
+      {isAuthenticated && (
+        <>
+          <Route path={APP_ROUTES.POLLS} element={<PollsList />} />
+          <Route path={APP_ROUTES.ADD_POLL} element={<CreatePoll />} />
+          <Route path={APP_ROUTES.EDIT_POLL.PATH} element={<CreatePoll />} />
+        </>
+      )}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
