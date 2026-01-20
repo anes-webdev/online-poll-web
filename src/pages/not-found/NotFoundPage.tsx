dc002febd055f6a693876f3d9c664fb9dc67474f
anes-webdev <anes.webdev@gmail.com>
2026-01-20 19:55:31 +0330

    feat: update not found page

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,14 +1,22 @@
-import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import { ErrorSection } from '../../components/ErrorSection/ErrorSection';
+import { useNavigate } from 'react-router';
 
 // Todo: add an icon here:
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Typography className="text-center mt-20" variant="h5" color="textMuted">
-        Page Not Found
-      </Typography>
-    </div>
+    <ErrorSection message="Page Not Found">
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Go Back
+      </Button>
+    </ErrorSection>
   );
 };
 export default NotFoundPage;
