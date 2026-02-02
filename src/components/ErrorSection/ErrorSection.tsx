9b7cbb860a5fde9177c3bdf01106b896cf44b679
anes-webdev <anes.webdev@gmail.com>
2026-02-02 17:09:17 +0330

    feat: update error section image

--- src/components/ErrorSection/ErrorSection.tsx
+++ src/components/ErrorSection/ErrorSection.tsx
@@ -1,5 +1,5 @@
 import { Typography } from '@mui/material';
-import NotFoundIcon from '../../assets/not-found.jpeg';
+import NotFoundIcon from '../../assets/not-found.jpg';
 import type { ReactNode } from 'react';
 
 type ErrorSectionProps = {
@@ -9,9 +9,13 @@ type ErrorSectionProps = {
 
 export const ErrorSection = ({ message, children }: ErrorSectionProps) => {
   return (
-    <div className="mx-auto max-w-md">
+    <div className="mx-auto max-w-lg">
       <img src={NotFoundIcon} alt="Not found icon" className="w-full"></img>
-      <Typography variant="h5" className="text-center" color="textSecondary">
+      <Typography
+        variant="h5"
+        className="text-center font-light! mt-2!"
+        color="textMuted"
+      >
         {message}
       </Typography>
       <div className="mt-7 flex justify-center">{children}</div>
