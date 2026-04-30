aa0b220c5c3ad8640ec04607f0f494985b8e18de
anes-webdev <anes.webdev@gmail.com>
2026-04-30 10:30:21 +0330

    chore: remove test component

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router';
 import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
 import { APP_ROUTES } from '../constants/routes';
-import { TestComponent } from '../pages/test/TestComponent';
 import { lazy, Suspense } from 'react';
 import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
 
@@ -12,7 +11,7 @@ const PollView = lazy(() => import('../pages/poll/view'));
 const PollsList = lazy(() => import('../pages/poll/list'));
 const CreatePoll = lazy(() => import('../pages/poll/add'));
 const NotFoundPage = lazy(() => import('../pages/not-found/NotFoundPage'));
-// Todo: remove test component:
+
 const AppRoutes = () => {
   return (
     <Suspense fallback={<LoadingSpinner />}>
@@ -21,7 +20,6 @@ const AppRoutes = () => {
         <Route path={APP_ROUTES.LANDING} element={<Landing />} />
         <Route path={APP_ROUTES.POLL_LINK.PATH} element={<PollLink />} />
         <Route path={APP_ROUTES.POLL_VIEW.PATH} element={<PollView />} />
-        <Route path={'test'} element={<TestComponent />} />
         <Route element={<ProtectedRoute />}>
           <Route path={APP_ROUTES.POLLS} element={<PollsList />} />
           <Route path={APP_ROUTES.ADD_POLL} element={<CreatePoll />} />
