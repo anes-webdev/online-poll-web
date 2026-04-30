49065285e155ba35e0f041852295fbf982e66a05
anes-webdev <anes.webdev@gmail.com>
2026-04-30 10:54:05 +0330

    chore: comment linked in icon

--- src/constants/footerLinks.ts
+++ src/constants/footerLinks.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import type { OverridableComponent } from '@mui/material/OverridableComponent';
 import type { SvgIconTypeMap } from '@mui/material';
 
@@ -25,10 +24,10 @@ export const FOOTER_LINKS: FooterLinkType[] = [
     tooltipTitle: 'Git Hub',
     icon: GitHubIcon,
   },
-  {
-    id: 'linkedin',
-    href: 'http://linkedin.com/in/anes-hekmatshoar-837557196',
-    tooltipTitle: 'Linked In',
-    icon: LinkedInIcon,
-  },
+  // {
+  //   id: 'linkedin',
+  //   href: 'http://linkedin.com/in/anes-hekmatshoar-837557196',
+  //   tooltipTitle: 'Linked In',
+  //   icon: LinkedInIcon,
+  // },
 ];
