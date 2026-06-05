14ed833aaa380137859bd70ffcc8c80b5708275d
anes-webdev <anes.webdev@gmail.com>
2026-06-05 20:34:19 +0330

    fix: web socket base url issue in production

--- src/constants/baseUrls.ts
+++ src/constants/baseUrls.ts
@@ -2,4 +2,5 @@ export const API_BASE_URL = import.meta.env.PROD
   ? '/api'
   : import.meta.env.VITE_API_BASE_URL;
 
+export const SOCKET_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const APP_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
